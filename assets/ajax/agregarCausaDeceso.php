bb6f2db20b8f06d5d8498161dc74e98cdbc081d1
eldanyruiz7 <gamb2006@gmail.com>
2019-10-31 22:52:55 -0600

    Creación clase Query, la cuál sirve como modelo para comunicar con bd.

--- assets/ajax/agregarCausaDeceso.php
+++ assets/ajax/agregarCausaDeceso.php
@@ -12,7 +12,8 @@
     else
     {
         require "../php/responderJSON.php";
-        require ("../php/usuario.class.php");
+		require ("../php/usuario.class.php");
+        require ("../php/query.class.php");
         $usuario = new usuario($idUsuario,$mysqli);
         $permiso = $usuario->permiso("agregarDifunto",$mysqli);
         if (!$permiso)
@@ -39,70 +40,45 @@
             responder($response, $mysqli);
         }
         $sql = "SELECT nombre FROM cat_causasdecesos WHERE nombre = ? AND activo = 1";
-        if ($prepare_select = $mysqli->prepare($sql))
-        {
-            if (!$prepare_select->bind_param('s',$nombre))
-            {
-                $response['mensaje'] = "No se puede consultar la información. Error al enlazar parámetros";
-                $response['status'] = 0;
-                responder($response, $mysqli);
-            }
-            if (!$prepare_select->execute())
-            {
-                $response['mensaje'] = "No se puede consultar la información. Error al ejecutar los parámetros";
-                $response['status'] = 0;
-                responder($response, $mysqli);
-            }
-            $res_select = $prepare_select->get_result();
-            if ($res_select->num_rows > 0)
-            {
-                $response['mensaje'] = "No se puede agregar esta causa de deceso. Ya existe registrada una con el mismo nombre. Elije otra distinta";
-                $response['status'] = 0;
-                responder($response, $mysqli);
-            }
-            else
-            {
-                $sql = "INSERT INTO cat_causasdecesos (nombre, usuario) VALUES (?,?)";
-                if ($prepare_insert = $mysqli->prepare($sql))
-                {
-                    if (!$prepare_insert->bind_param("si",$nombre,$idUsuario))
-                    {
-                        $response['mensaje'] = "No se pudo guardar la información. Error al enlazar parámetros";
-                        $response['status'] = 0;
-                        responder($response, $mysqli);
-                    }
-                    if (!$prepare_insert->execute())
-                    {
-                        $response['mensaje'] = "No se pudo guardar la información. Error al ejecutar parámetros";
-                        $response['status'] = 0;
-                        responder($response, $mysqli);
-                    }
-                    else
-                    {
-						// Agregar evento en la bitácora de eventos
-						$idUsuario 				= $sesion->get("id");
-						$ipUsuario 				= $sesion->get("ip");
-						$pantalla				= "Agregar/Modificar difunto";
-						$descripcion			= "Se agregó una nueva causa de deceso ($nombre) al catálogo de causas de decesos";
-						$sql					= "CALL agregarEvento($idUsuario, '$ipUsuario', '$pantalla', '$descripcion', $idSucursal);";
-						$mysqli					->query($sql);
-                        $response['mensaje'] 	= "Nueva causa de deceso generada correctamente.";
-                        $response['status'] 	= 1;
-                        responder($response, $mysqli);
-                    }
-                }
-                else
-                {
-                    $response['mensaje'] = "No se puede guardar la información. Error al preparar los parámetros";
-                    $response['status']         = 0;
+		$params= array('s',$nombre);
+		$query = new Query();
+		if ($query ->sentence($sql, $params))
+		{
+	        if ($query->num_rows())
+	        {
+	            $response['mensaje'] = "No se puede agregar esta causa de deceso. Ya existe registrada una con el mismo nombre. Elije otra distinta";
+	            $response['status'] = 0;
+	            responder($response, $mysqli);
+	        }
+	        else
+	        {
+	            $sql = "INSERT INTO cat_causasdecesos (nombre, usuario) VALUES (?,?)";
+				$params = array("si",$nombre,$idUsuario);
+				if ($query ->sentence($sql, $params) && $query ->affected_rows())
+				{
+					// Agregar evento en la bitácora de eventos
+					$idUsuario 				= $sesion->get("id");
+					$ipUsuario 				= $sesion->get("ip");
+					$pantalla				= "Agregar/Modificar difunto";
+					$descripcion			= "Se agregó una nueva causa de deceso ($nombre) al catálogo de causas de decesos";
+					$sql					= "CALL agregarEvento($idUsuario, '$ipUsuario', '$pantalla', '$descripcion', $idSucursal);";
+					$mysqli					->query($sql);
+                    $response['mensaje'] 	= "Nueva causa de deceso generada correctamente.";
+                    $response['status'] 	= 1;
                     responder($response, $mysqli);
-                }
-            }
-        }
+	            }
+	            else
+	            {
+					$response['mensaje'] 		= $query ->mensaje();
+	                $response['status']         = 0;
+	                responder($response, $mysqli);
+	            }
+	        }
+		}
         else
         {
-            $response['mensaje'] = "No se puede consultar la información. Error al preparar los parámetros";
-            $response['status']         = 0;
+			$response['mensaje'] = $query ->mensaje();
+            $response['status'] = 0;
             responder($response, $mysqli);
         }
     }
---
+++ assets/php/query.class.php
@@ -0,0 +1,119 @@
+<?php
+/**
+* CLASE Query
+* Esta clase sirve como Modelo, funciona para comunicar
+* direcatmente con la base de datos
+*
+*
+**/
+class Query
+{
+	/**
+	*
+	* Obtiene el tipo de query (Select, Insert, Update)
+	*
+	*
+	**/
+	private function obtenerTipoQuery()
+	{
+		if (stripos($this ->sql, 'select') !== false) {
+	    	$tipo = "consultar";
+		}
+		elseif (stripos($this ->sql, 'insert') !== false) {
+	    	$tipo = "guardar";
+		}
+		elseif (stripos($this ->sql, 'update') !== false) {
+	    	$tipo = "actualizar";
+		}
+		else
+			$tipo = "enlazar";
+
+		return $tipo;
+	}
+	/**
+	*
+	*
+	* @var string String con la consulta SQL
+	* @var array Array con parámetros, tipos y variables. Ejem: ("is","id, nombre")
+	*
+	*
+	* $params= array("ss","string_1","string_2");
+	*
+	**/
+    public function sentence($sql, $params)
+    {
+		//connect
+		global $mysqli;
+		$response = array();
+		$this ->mensaje = NULL;
+		$this ->status = 0;
+		$this ->num_rows = 0;
+		$this ->affected_rows = 0;
+		$this ->data = 0;
+		$this ->sql = $sql;
+		//prepare
+		if($prepare_select = $mysqli->prepare($sql))
+		{
+			$tmp = array();
+
+			$tipo = $this ->obtenerTipoQuery();
+			foreach($params as $key => $value) $tmp[$key] = &$params[$key];
+			call_user_func_array(array($prepare_select, 'bind_param'), $tmp);
+
+			if(!$prepare_select->execute())
+			{
+				$this ->mensaje = "No se puede $tipo la información. Error al ejecutar los parámetros";
+                return false;
+			}
+			else
+			{
+				$a_data = array();
+				if ($tipo == 'guardar' || $tipo == 'actualizar')
+				{
+					$this ->affected_rows = $prepare_select ->affected_rows;
+
+				}
+				else
+				{
+					$res_select = $prepare_select->get_result();
+					$this ->num_rows = $res_select ->num_rows;
+					while($row = $res_select->fetch_array(MYSQLI_ASSOC)) {
+						$a_data[]=$row;
+					}
+					$this ->data = $a_data;
+				}
+				$this ->mensaje = "Sentencia realizada con éxito";
+				$this ->status = 1;
+				$prepare_select ->close();
+				return true;
+			}
+		}
+		else
+		{
+			$this ->mensaje = "No se puede $tipo la información. Error al preparar los parámetros";
+			return false;
+		}
+
+
+    }
+	public function mensaje()
+	{
+		return $this->mensaje;
+	}
+	public function num_rows()
+	{
+		return $this->num_rows;
+	}
+	public function affected_rows()
+	{
+		return $this->affected_rows;
+	}
+	public function data()
+	{
+		return $this->data;
+	}
+	public function status()
+	{
+		return $this->status;
+	}
+}
