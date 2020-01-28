c3bde06e059bd5e5486383dea09531e817ac1d0a
eldanyruiz7 <gamb2006@gmail.com>
2020-01-27 23:57:06 -0600

    Correcciones varias

--- assets/php/query.class.php
+++ assets/php/query.class.php
@@ -377,6 +377,104 @@ class Query
 			return false;
 		}
 	}
+	/**
+	 * DB Backup
+	 */
+	public function backup($tables = "*")
+	{
+		//ENTER THE RELEVANT INFO BELOW
+    // $mysqlUserName      = "Your Username";
+    // $mysqlPassword      = "Your Password";
+    // $mysqlHostName      = "Your Host";
+    // $DbName             = "Your Database Name here";
+    // $backup_name        = "mybackup.sql";
+    // $tables             = "Your tables";
+
+   //or add 5th parameter(array) of specific tables:    array("mytable1","mytable2","mytable3") for multiple tables
+
+    // Export_Database($mysqlHostName,$mysqlUserName,$mysqlPassword,$DbName,  $tables=false, $backup_name=false );
+
+    // function Export_Database($host,$user,$pass,$name,  $tables=false, $backup_name=false )
+    // {
+    	// $this ->query()
+        // $mysqli = new mysqli($host,$user,$pass,$name);
+        // $mysqli->select_db($name);
+        // $mysqli->query("SET NAMES 'utf8'");
+        $backup_name = FALSE;
+        $this ->restartParam();
+		self::$mysqli->query("SET NAMES 'utf8'");
+
+		$queryTables = self::$mysqli->query('SHOW TABLES FROM '.$this->dataBase);
+		// print_r($queryTables);
+		while($row = $queryTables->fetch_row())
+		{
+			$target_tables[] = $row[0];
+		}
+		// var_dump($target_tables);
+		$tables = false;
+		if($tables !== false)
+		{
+			$target_tables = array_intersect( $target_tables, $tables);
+		}
+		$content = "SET FOREIGN_KEY_CHECKS=0;\n";
+		foreach($target_tables as $table)
+		{
+			$result = self::$mysqli->query('SELECT * FROM '.$table);
+			$fields_amount = $result->field_count;
+			$rows_num = self::$mysqli->affected_rows;
+			$res = self::$mysqli->query('SHOW CREATE TABLE '.$table);
+			// self::$mysqli->query("SET FOREIGN_KEY_CHECKS=0;");
+			$TableMLine = $res->fetch_row();
+			$content = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
+
+			for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0)
+			{
+				while($row = $result->fetch_row())
+				{
+					if ($st_counter%100 == 0 || $st_counter == 0)
+					{
+						$content .= "\nINSERT INTO ".$table." VALUES";
+					}
+					$content .= "\n(";
+					for($j=0; $j<$fields_amount; $j++)
+					{
+						$row[$j] = str_replace("\n","\\n", addslashes($row[$j]) );
+						if (isset($row[$j]))
+						{
+							$content .= '"'.$row[$j].'"' ;
+						}
+						else
+						{
+							$content .= '""';
+						}
+						if ($j<($fields_amount-1))
+						{
+							$content.= ',';
+						}
+					}
+					$content .=")";
+
+					if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num)
+					{
+						$content .= ";";
+					}
+					else
+					{
+						$content .= ",";
+					}
+					$st_counter=$st_counter+1;
+				}
+			} $content .="\n\n\n";
+		}
+		// self::$mysqli->query("SET FOREIGN_KEY_CHECKS=1;");
+
+		$backup_name = $backup_name ? $backup_name : $this->dataBase."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
+		$backup_name = $backup_name ? $backup_name : $this->dataBase.".sql";
+		header('Content-Type: application/octet-stream');
+		header("Content-Transfer-Encoding: Binary");
+		header("Content-disposition: attachment; filename=\"".$backup_name."\"");
+		return $content;
+	}
 	//////////////////////// MIGRATIONS ///////////////////////
 	public function createTable($table, $replace = TRUE)
 	{
